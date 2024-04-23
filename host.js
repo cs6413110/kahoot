ab1bd3f1726596456157cb1067014175005bda81
GamingCruizer <125952701+GamingCruizer@users.noreply.github.com>
2024-04-23 12:30:15 -0700

    Update host.js

--- host.js
+++ host.js
@@ -6,11 +6,10 @@ We can still use CSS, but it will be executed via js.
 Write your html code in a seperate file and we can implement tomorrow.
 
 
-
-
-
 So don't use the .html file? or don't create a .css? Or what else do I use?
 
 A: We are going to combine them all in a js file. You can start by doing whatever you want and I'll combine it tomorrow.
+
+Q: After I program something, how do I view it as an actual page?
 
 */
