791e4703dd7967c39184e720b0cfb504580b5048
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-04-23 09:21:09 -0700

    simple echo server initial

---
+++ server.js
@@ -0,0 +1,18 @@
+
+const ws = require('ws');
+const wss = new ws.WebSocketServer({port: 3801});
+
+const rooms = {};
+
+wss.on('connection', socket => {
+  socket.on('message', data => {
+    socket.send(data);
+    if (data.event === 'join') {
+      
+    } else if (data.event === 'host') {
+      
+    } else if (data.event === 'answer') {
+      
+    }
+  });
+});
