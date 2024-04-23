f78cd3d1f670cbc12f5debe85dfaf203cb04a211
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-04-23 09:42:08 -0700

    Create host.js

---
+++ host.js
@@ -0,0 +1,9 @@
+/*
+Actually seth we will be using js files because the school blocked static github hosting and even if they didn't ssl prevents server communication without a domain.
+
+We can still use CSS, but it will be executed via js.
+
+Write your html code in a seperate file and we can implement tomorrow.
+
+
+*/
