54caa389a2336286b88208ba36ef45cf3e586676
GamingCruizer <125952701+GamingCruizer@users.noreply.github.com>
2024-04-23 09:49:50 -0700

    Update host.js

--- host.js
+++ host.js
@@ -5,5 +5,7 @@ We can still use CSS, but it will be executed via js.
 
 Write your html code in a seperate file and we can implement tomorrow.
 
+So dont use the .html file? or dont create a .css? or what else do I use?
+
 
 */
