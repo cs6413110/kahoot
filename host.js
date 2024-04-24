66f1a13cb17facbe001d40c62a229a0fac9390e6
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-04-24 08:07:35 -0700

    Update host.js

--- host.js
+++ host.js
@@ -74,6 +74,7 @@ socket._send = socket.send;
 socket.send = m => socket._send(JSON.stringify(m))
 socket.onopen = () => alert('Connected to server!');
 socket.onmessage = d => {
+  alert(d.data);
   const data = JSON.parse(d.data);
   alert(data);
   if (data.event === 'code') {
