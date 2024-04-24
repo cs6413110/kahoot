4630dbaf7c1612d8bcd3007fae78610da835937e
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-04-24 07:48:27 -0700

    basic host interface setup

--- host.js
+++ host.js
@@ -1,72 +1,97 @@
 document.body.innerHTML += `
-<!-- Host Side -->
-
-
 <DOCTYPE html>
-  <html lang="en-US"> 
-    <header>
-      <title>Kahooty Rip-off</title>
-    </header>
-    
-    <body>
-      <div class="container">
-        <div class="question">Question: </div>
-
-        <div class="options">
-          <div class="option">Answer 1:</div>
-          <div class="option">Answer 2:</div>
-          <div class="option">Answer 3:</div>
-          <div class="option">Answer 4</div>
-        </div>
-
-        <div class="timer">Timer: 00:00</div>
+<html lang="en-US"> 
+  <header>
+    <title>Kahooty Rip-off</title>
+  </header>
+  <body>
+    <div class='initial'>
+      <button onclick='createGame()'>Host a Game</button>
+    </div>
+    <div class="container">
+      <div class="question">Question: </div>
+      <div class='roomID'>asdf</div>
+      <div class="options">
+        <div class="option">Answer 1:</div>
+        <div class="option">Answer 2:</div>
+        <div class="option">Answer 3:</div>
+        <div class="option">Answer 4</div>
       </div>
-    </body>  
+      <div class="timer">Timer: 00:00</div>
+    </div>
+  </body>  
 
-    <style>
-      .container {
-        position: relative;
-        height: 100vh;
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-end;
-      }
-      .question {
-        position: absolute;
-        top: 10px;
-        left: 50%;
-        transform: translateX(-50%);
-        padding: 10px;
-        margin: 10px;
-        background-color: #f0f0f0;
-        border: 2px solid #ccc;
-        border-radius: 8px;
-      }
-      .options {
-        display: flex;
-        flex-direction: column;
-        margin-bottom: 50px;
-      }
-      .option {
-        padding: 10px;
-        margin: 5px;
-        background-color: #f0f0f0;
-        border: 1px solid #ccc;
-        border-radius: 8px;
-      }
-      .option:hover {
-        transform: scale(0.9);
-        cursor: pointer;
-      }
-      .timer {
-        position: absolute;
-        top: 10px;
-        right: 10px;
-        background-color: #f0f0f0;
-        padding: 5px 10px;
-        border-radius: 5px;
-      }
-      
-    </style>
-  </html>
+  <style>
+    .container {
+      position: relative;
+      height: 100vh;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+    }
+    .question {
+      position: absolute;
+      top: 10px;
+      left: 50%;
+      transform: translateX(-50%);
+      padding: 10px;
+      margin: 10px;
+      background-color: #f0f0f0;
+      border: 2px solid #ccc;
+      border-radius: 8px;
+    }
+    .options {
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 50px;
+    }
+    .option {
+      padding: 10px;
+      margin: 5px;
+      background-color: #f0f0f0;
+      border: 1px solid #ccc;
+      border-radius: 8px;
+    }
+    .option:hover {
+      transform: scale(0.9);
+      cursor: pointer;
+    }
+    .timer {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      background-color: #f0f0f0;
+      padding: 5px 10px;
+      border-radius: 5px;
+    }  
+  </style>
+</html>
 `;
+
+const socket = new WebSocket('ws://141.148.128.231:443'); // connect to server
+const game = {};
+socket._send = socket.send;
+socket.send = m => socket._send(JSON.stringify(m))
+socket.onopen = () => alert('Connected to server!');
+socket.onmessage = d => {
+  const data = JSON.parse(d.data);
+  if (data.event === 'code') {
+    info.code = data.code;
+    document.getElementById('roomID').innerHTML = data.code;
+  }
+}
+socket.onclose = () => alert('Disconnected from server! Please reload.');
+
+const createGame = () => {
+  socket.send({event: 'host', questions: prompt('Input JSON data for game: ')});
+  
+  /*
+  JSON format for questions:
+  [{question: '', answers: ['a', 'b', 'c', 'd'], correct: 0(a), time: 30}]
+  */
+  // hide the creategame button
+  // load the lobby
+  // add handlers for beginGame
+  
+}
+
