5e47399597f3e7ee6976b8fc580dc8d5d64da46f
GamingCruizer <125952701+GamingCruizer@users.noreply.github.com>
2024-05-03 09:32:37 -0700

    Update host.js

--- host.js
+++ host.js
@@ -7,7 +7,7 @@ document.body.innerHTML += `
   <div class='hostRoom'>
     <span id='playercount'>0 Players</span> 
     <span id='roomID'>XXXXXX</span>
-    <div><buttononclick='startGame()' class="pointer">Start Game</button></div>
+    <div><button onclick='startGame()' class="pointer">Start Game</button></div>
   </div>
   <div class="line"></div>
   <div class='playerDisplay' id='playerlist'></div>
